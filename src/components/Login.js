5d6d1452c1a4a71aad254807c5f042e5578d3b27
yurioneix <yurioneix18@gmail.com>
2023-03-02 17:54:11 -0300

    Requisitos 1, 2, 3 e 4: resolvido

---
+++ src/components/Login.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    const validateLogin = () => {
+      const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+      const limit = 6;
+      console.log(email, password);
+      setIsDisabled(!(regex.test(email) && password.length > limit));
+    };
+    validateLogin();
+  }, [email, password]);
+
+  return (
+    <form>
+      <label htmlFor="email-input">
+        Email
+        <input
+          id="email-input"
+          data-testid="email-input"
+          type="email"
+          value={ email }
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+      </label>
+      <label htmlFor="password-input">
+        Password
+        <input
+          id="password-input"
+          data-testid="password-input"
+          type="password"
+          value={ password }
+          onChange={ (e) => setPassword(e.target.value) }
+        />
+      </label>
+      <button
+        data-testid="login-submit-btn"
+        type="button"
+        disabled={ isDisabled }
+      >
+        Enter
+
+      </button>
+    </form>
+  );
+}
+
+export default Login;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testa componente Login', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+  it('Testa se é exibido na tela de Login os campos de email, senha e o botão de Enter', () => {
+    const emailInput = screen.getByLabelText(/Email/i);
+    const passwordInput = screen.getByLabelText(/Password/i);
+    const button = screen.getByRole('button', { name: /Enter/i });
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+  it('Testa se o botão de Entrar só é habilitado quando é digitado um email válido e senha', () => {
+    const emailInput = screen.getByLabelText(/Email/i);
+    const passwordInput = screen.getByLabelText(/Password/i);
+    const button = screen.getByRole('button', { name: /Enter/i });
+
+    expect(button).not.toBeEnabled();
+
+    userEvent.type(emailInput, 'email@email.com');
+    userEvent.type(passwordInput, '12345678');
+    expect(button).toBeEnabled();
+
+    // userEvent.click(button);
+  });
 });
