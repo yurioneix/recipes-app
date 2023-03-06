58642455ef960c3fbd61c8e13dad553601ecdcf9
FabianoCarvalho <fabianolucas27@hotmail.com>
2023-03-06 15:04:06 -0400

    Requisitos 57, 58, 59 renderização email usuário, implementação dos botões.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,19 +1,46 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import HeaderContext from '../context/HeaderContext';
 
 export default function Profile() {
   const { setTitle, setShowHeader, setSearch } = useContext(HeaderContext);
+  
+  const [userEmail, setUserEmail] = useState('');
 
   useEffect(() => {
     setShowHeader(true);
     setTitle('Profile');
     setSearch(false);
+    setUserEmail(JSON.parse(localStorage.getItem('user')));
   }, [setShowHeader, setTitle, setSearch]);
 
   return (
     <div>
       Profile
+
+      <h2 data-testid="profile-email">
+        { userEmail.email }
+      </h2>
+
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
+
       <Footer />
     </div>
   );
