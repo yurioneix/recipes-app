01fe05852f26342c46283e765685f5a89082d0bb
Douglas Santos <ds4208151@gmail.com>
2023-03-06 17:55:41 -0300

    req 37 completo

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,11 +1,15 @@
-import React, { useContext, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import HeaderContext from '../context/HeaderContext';
 import RecipesContext from '../context/RecipesContext';
+import { fetchDetails } from '../services/fetchRecipes';
 
 export default function RecipeInProgress() {
+  const { id } = useParams();
   const { setShowType } = useContext(RecipesContext);
   const { setShowHeader } = useContext(HeaderContext);
+  const [details, setDetails] = useState({});
+
   const {
     location: { pathname },
   } = useHistory();
@@ -21,7 +25,30 @@ export default function RecipeInProgress() {
     }
   }, [setShowType, setShowHeader, pathname]);
 
+
+  useEffect(() => {
+    const getDatails = async () => {
+      const result =  await fetchDetails(pathname, id)
+      setDetails(result)
+    }
+    getDatails();
+    }, [])
+
+  useEffect(() => console.log(details),[details])
+
   return (
-    <div>RecipeInProgress</div>
+    <main>
+  <img src="recipe-photo.jpg" alt="Recipe Photo" data-testid="recipe-photo" />
+      <h1 data-testid="recipe-title">Delicious Recipe</h1>
+      <button data-testid="share-btn">Share</button>
+      <button data-testid="favorite-btn">Favorite</button>
+      <p data-testid="recipe-category">Main Course</p>
+      <div data-testid="instructions">
+        <p>Step 1: Do this</p>
+        <p>Step 2: Do that</p>
+        <p>Step 3: Enjoy!</p>
+      </div>
+      <button data-testid="finish-recipe-btn">Finish Recipe</button>
+    </main>
   );
 }
