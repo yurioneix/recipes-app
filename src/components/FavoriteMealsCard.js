88fe061446792eb08b29a8eebc78ce03dc848013
yurioneix <yurioneix18@gmail.com>
2023-03-10 17:46:56 -0300

    Requisito 53: cria função que ao clicar no botão de compartilhar, exibe a mensagem Link copied! e gera um link para página de detalhes da receita

--- src/components/FavoriteMealsCard.js
+++ src/components/FavoriteMealsCard.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
+// import { handleCopy } from '../services/utils';
 
 export default function FavoriteRecipesCard(props) {
   const { favorite, index } = props;
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = (callback, id) => {
+    callback(true);
+    const url = window.location.href.replace('/favorite-recipes', `/meals/${id}`);
+    navigator.clipboard.writeText(url);
+  };
+
   return (
     <div key={ favorite.id }>
       <p data-testid={ `${index}-horizontal-name` }>
@@ -20,13 +29,14 @@ export default function FavoriteRecipesCard(props) {
         src={ favorite.image }
         data-testid={ `${index}-horizontal-image` }
       />
-      <button>
+      <button onClick={ () => handleCopy(setCopied, favorite.id) }>
         <img
           src={ shareIcon }
           alt={ favorite.name }
           data-testid={ `${index}-horizontal-share-btn` }
         />
       </button>
+      {copied && <span>Link copied!</span>}
       <button>
         <img
           src={ blackHeartIcon }
