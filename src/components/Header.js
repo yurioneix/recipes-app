e0c6ae40a8ed42e5ac26a5fc50a1aebcfdd7687b
Douglas Santos <ds4208151@gmail.com>
2023-03-12 06:15:25 -0300

    tela de login padronizada

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,37 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import HeaderContext from '../context/HeaderContext';
-import profileIcon from '../images/profileIcon.svg';
-import searchIcon from '../images/searchIcon.svg';
-import SearchBar from './SearchBar';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import HeaderContext from "../context/HeaderContext";
+import profileIcon from "../images/profileIcon.svg";
+import searchIcon from "../images/searchIcon.svg";
+import SearchBar from "./SearchBar";
 
 export default function Header() {
   const { title, search, searchBar, setSearchBar } = useContext(HeaderContext);
 
   return (
-    <div>
-      <h1 data-testid="page-title">{title}</h1>
-      <Link to="/profile">
-        <img src={ profileIcon } alt="user Icon" data-testid="profile-top-btn" />
-      </Link>
-      {search && (
-        <button onClick={ () => setSearchBar(!searchBar) } data-testid="btn-search">
-          <img src={ searchIcon } alt="search Icon" data-testid="search-top-btn" />
-
-        </button>
-      )}
+    <div className="flex-flex-col mt-2">
+      <div className="flex w-full justify-around">
+        <Link to="/profile">
+          <img
+            src={profileIcon}
+            alt="user Icon"
+            data-testid="profile-top-btn"
+          />
+        </Link>
+        <h1 data-testid="page-title" className="text-3xl text-red-400">{title}</h1>
+        {search && (
+          <button
+            onClick={() => setSearchBar(!searchBar)}
+            data-testid="btn-search"
+          >
+            <img
+              src={searchIcon}
+              alt="search Icon"
+              data-testid="search-top-btn"
+            />
+          </button>
+        )}
+      </div>
       {searchBar && search && <SearchBar />}
     </div>
   );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,55 +1,72 @@
-import React, { useContext, useCallback, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import HeaderContext from '../context/HeaderContext';
-import RecipesContext from '../context/RecipesContext';
-import { fetchRecipes } from '../services/fetchRecipes';
+import React, { useContext, useCallback, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import HeaderContext from "../context/HeaderContext";
+import RecipesContext from "../context/RecipesContext";
+import { fetchRecipes } from "../services/fetchRecipes";
 
 export default function SearchBar() {
-  const { selected, setSelected, setResult, result } = useContext(HeaderContext);
+  const { selected, setSelected, setResult, result } =
+    useContext(HeaderContext);
   const { showType } = useContext(RecipesContext);
   const history = useHistory();
 
-  const handleFilter = useCallback(async () => { // faz requisições a api através do que o usuário digitou e selecionou na SearchBar. Quando clica no botão search dispara essa função
-    if (selected.searchRadio === 'firstLetter' && selected.searchInput.length > 1) {
-      return global.alert('Your search must have only 1 (one) character');
+  const handleFilter = useCallback(async () => {
+    // faz requisições a api através do que o usuário digitou e selecionou na SearchBar. Quando clica no botão search dispara essa função
+    if (
+      selected.searchRadio === "firstLetter" &&
+      selected.searchInput.length > 1
+    ) {
+      return global.alert("Your search must have only 1 (one) character");
     }
-    if (showType === 'meal') {
-      if (selected.searchRadio === 'ingredient') {
-        setResult(await fetchRecipes(
-          `https://www.themealdb.com/api/json/v1/1/filter.php?i=${selected.searchInput}`,
-        ));
-      } else if (selected.searchRadio === 'name') {
-        setResult(await fetchRecipes(
-          `https://www.themealdb.com/api/json/v1/1/search.php?s=${selected.searchInput}`,
-        ));
+    if (showType === "meal") {
+      if (selected.searchRadio === "ingredient") {
+        setResult(
+          await fetchRecipes(
+            `https://www.themealdb.com/api/json/v1/1/filter.php?i=${selected.searchInput}`
+          )
+        );
+      } else if (selected.searchRadio === "name") {
+        setResult(
+          await fetchRecipes(
+            `https://www.themealdb.com/api/json/v1/1/search.php?s=${selected.searchInput}`
+          )
+        );
       } else {
-        setResult(await fetchRecipes(
-          `https://www.themealdb.com/api/json/v1/1/search.php?f=${selected.searchInput}`,
-        ));
+        setResult(
+          await fetchRecipes(
+            `https://www.themealdb.com/api/json/v1/1/search.php?f=${selected.searchInput}`
+          )
+        );
       }
-    } else if (showType === 'drinks') {
-      if (selected.searchRadio === 'ingredient') {
-        setResult(await fetchRecipes(
-          `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${selected.searchInput}`,
-        ));
-      } else if (selected.searchRadio === 'name') {
-        setResult(await fetchRecipes(
-          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${selected.searchInput}`,
-        ));
+    } else if (showType === "drinks") {
+      if (selected.searchRadio === "ingredient") {
+        setResult(
+          await fetchRecipes(
+            `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${selected.searchInput}`
+          )
+        );
+      } else if (selected.searchRadio === "name") {
+        setResult(
+          await fetchRecipes(
+            `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${selected.searchInput}`
+          )
+        );
       } else {
-        setResult(await fetchRecipes(
-          `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${selected.searchInput}`,
-        ));
+        setResult(
+          await fetchRecipes(
+            `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${selected.searchInput}`
+          )
+        );
       }
     }
   }, [selected.searchRadio, selected.searchInput, setResult, showType]);
 
   useEffect(() => {
     if (result.drinks?.length === 1 || result.meals?.length === 1) {
-      if (showType === 'meal') {
+      if (showType === "meal") {
         const { idMeal } = result.meals[0];
         history.push(`/meals/${idMeal}`);
-      } else if (showType === 'drinks') {
+      } else if (showType === "drinks") {
         const { idDrink } = result.drinks[0];
         history.push(`/drinks/${idDrink}`);
       }
@@ -57,66 +74,76 @@ export default function SearchBar() {
   }, [showType, result, history]);
 
   return (
-    <form>
-      <div>SearchBar</div>
-      <label htmlFor="searchInput">
-        <input
-          id="searchInput"
-          type="text"
-          data-testid="search-input"
-          value={ selected.searchInput }
-          onChange={ (e) => setSelected({ ...selected, searchInput: e.target.value }) }
-        />
-      </label>
-      <label
-        htmlFor="ingredient"
-      >
-        <input
-          type="radio"
-          id="ingredient"
-          data-testid="ingredient-search-radio"
-          value="ingredient"
-          name="searchRadio"
-          onChange={ (e) => setSelected({ ...selected, searchRadio: e.target.value }) }
-        />
-        Ingredient
-      </label>
-      <label
-        htmlFor="nameSearch"
-      >
-        <input
-          type="radio"
-          id="nameSearch"
-          data-testid="name-search-radio"
-          value="name"
-          name="searchRadio"
-          onChange={ (e) => setSelected({ ...selected, searchRadio: e.target.value }) }
-        />
-        Name
-      </label>
-      <label
-        htmlFor="firstLetterSearch"
-      >
-        <input
-          type="radio"
-          id="firstLetterSearch"
-          data-testid="first-letter-search-radio"
-          value="firstLetter"
-          name="searchRadio"
-          onChange={ (e) => setSelected({ ...selected, searchRadio: e.target.value }) }
-        />
-        First letter
-      </label>
+    <form className="w-full">
+      <div>
+        <label htmlFor="searchInput">
+          <input
+            id="searchInput"
+            type="text"
+            className="border rounded-[4px] p-3 hover:outline-none focus:outline-none border-yellow-500 w-full mt-5"
+            data-testid="search-input"
+            value={selected.searchInput}
+            onChange={(e) =>
+              setSelected({ ...selected, searchInput: e.target.value })
+            }
+          />
+        </label>
+      </div>
+      <div className="flex w-full justify-around mt-3">
+        <label htmlFor="ingredient">
+          <input
+            type="radio"
+            id="ingredient"
+            data-testid="ingredient-search-radio"
+            value="ingredient"
+            name="searchRadio"
+            onChange={(e) =>
+              setSelected({ ...selected, searchRadio: e.target.value })
+            }
+          />
+          Ingredient
+        </label>
+        <label htmlFor="nameSearch">
+          <input
+            type="radio"
+            id="nameSearch"
+            data-testid="name-search-radio"
+            value="name"
+            name="searchRadio"
+            onChange={(e) =>
+              setSelected({ ...selected, searchRadio: e.target.value })
+            }
+          />
+          Name
+        </label>
+        <label htmlFor="firstLetterSearch">
+          <input
+            type="radio"
+            id="firstLetterSearch"
+            data-testid="first-letter-search-radio"
+            value="firstLetter"
+            name="searchRadio"
+            onChange={(e) =>
+              setSelected({ ...selected, searchRadio: e.target.value })
+            }
+          />
+          First letter
+        </label>
+      </div>
+
       <button
         type="button"
         data-testid="exec-search-btn"
-        onClick={ () => {
+        className="mt-5 w-full border p-2 bg-gradient-to-r
+         from-gray-800 bg-gray-500 text-white rounded-[4px]
+         hover:bg-slate-400 scale-105 duration-300 disabled:text-gray-500"
+        onClick={() => {
           handleFilter();
           setSelected({
-            searchInput: '',
-            searchRadio: '',
+            searchInput: "",
+            searchRadio: "",
           });
-        } }
+        }}
       >
         Search
       </button>
